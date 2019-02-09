7a92b0b2864bcd2f2317759184426e1aed34ed09
Gordon-Yeh <gordon.yeh.dev@gmail.com>
2019-02-08 21:26:40 -0800

    add textbox

---
+++ c_source/Graphics/components/textbox.h
@@ -0,0 +1,43 @@
+#ifndef LABEL_H
+#define LABEL_H
+
+#include "../shapes/box.h"
+
+#define LEFT 0
+#define CENTER 1
+#define RIGHT 2
+
+#define TOP 0
+#define MIDDLE 1
+#define BOTTOM 2
+
+typedef struct {
+    char *text;
+    int font;
+    int colour;
+    Box * box;
+
+    // TODO:
+    int v_align;
+    int h_align;
+} TextBox;
+
+TextBox * textbox_create(int x, int y, int l, int w);
+
+void textbox_destory(TextBox *tb);
+
+void textbox_set_text(TextBox *tb, char *text, int font, int colour);
+
+void textbox_set_box_colour(TextBox *tb, int b_c, int f_c);
+
+int textbox_get_length(TextBox *tb);
+int textbox_get_width(TextBox *tb);
+int textbox_get_x(TextBox *tb);
+int textbox_get_y(TextBox *tb);
+
+int textbox_within(TextBox *tb, Point p);
+
+// draw out the text on screen
+void textbox_draw(TextBox *);
+
+#endif
