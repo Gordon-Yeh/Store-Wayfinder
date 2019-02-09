1463dba53e10d74fdcf6ca22f41d22503c65ce6a
Gordon-Yeh <gordon.yeh.dev@gmail.com>
2019-02-08 21:26:40 -0800

    add textbox

--- c_source/Graphics/HomeScreen.c
+++ c_source/Graphics/HomeScreen.c
@@ -3,6 +3,7 @@
 #include "Screens.h"
 #include "Text.h"
 #include "Touchscreen.h"
+#include "components/textbox.h"
 #include "shapes/box.h"
 #include "draw/draw.h"
 
@@ -15,39 +16,40 @@ void ResetScreen(void) {
 
 void HomeScreen(void) {
 	ResetScreen();
-	
-	Box * new_list_box = create_box(30, 10, 760, 100, BLUE, FOREST_GREEN);
-	Box * add_to_list_box = create_box(30, 120, 760, 100, BLUE, FOREST_GREEN);
-	Box * map_box = create_box(30, 240, 760, 100, BLUE, FOREST_GREEN);
-	Box * help_box = create_box(30, 360, 760, 100, BLUE, FOREST_GREEN);
 
-	draw_box(new_list_box);
-	draw_box(add_to_list_box);
-	draw_box(map_box);
-	draw_box(help_box);
+	TextBox * new_list_button = textbox_create(30, 10, 760, 100);
+	TextBox * add_to_list_button = textbox_create(30, 120, 760, 100);
+	TextBox * map_button = textbox_create(30, 240, 760, 100);
+	TextBox * help_button = textbox_create(30, 360, 760, 100);
 
-	CenteredSentence(FONT2, 30, 789, 10, 109, WHITE, 0, "Create New Shopping List", DONT_ERASE);
-	CenteredSentence(FONT2, 30, 789, 120, 229, WHITE, 0, "Add To Shopping List", DONT_ERASE);
-	CenteredSentence(FONT2, 30, 789, 240, 349, WHITE, 0, "Go To Map", DONT_ERASE);
-	CenteredSentence(FONT2, 30, 789, 360, 469, WHITE, 0, "Help", DONT_ERASE);
-	
+	textbox_set_box_colour(new_list_button, BLUE, FOREST_GREEN);
+	textbox_set_box_colour(add_to_list_button, BLUE, FOREST_GREEN);
+	textbox_set_box_colour(map_button, BLUE, FOREST_GREEN);
+	textbox_set_box_colour(help_button, BLUE, FOREST_GREEN);
+
+	textbox_set_text(new_list_button, "Create New Shopping List", FONT2, WHITE);
+	textbox_set_text(add_to_list_button, "Add To Shopping List", FONT2, WHITE);
+	textbox_set_text(map_button, "Go To Map", FONT2, WHITE);
+	textbox_set_text(help_button, "Help", FONT2, WHITE);
+
+	textbox_draw(new_list_button);
+	textbox_draw(add_to_list_button);
+	textbox_draw(map_button);
+	textbox_draw(help_button);
+
 	//Touchscreen
 	Point p, p1;
 	while (1) {
 		p = GetPress();
-		printf("Press: x = %d, y = %d\n", p.x, p.y);
 		p1 = GetRelease(); //wait for a getrelease
-		printf("Release: x = %d, y = %d\n", p1.x, p1.y);
-		if (within_box(new_list_box, p)) {
-			// ItemScreen();
-			break;
-		} else if (within_box(add_to_list_box, p)) {
+		if (textbox_within(new_list_button, p1)) {
+			// TODO:
+		} else if (textbox_within(add_to_list_button, p1)) {
 			ItemScreen();
-			break;
-		} else if (within_box(map_box, p)) {
-			break;
-		} else if (within_box(help_box, p)) {
-			break;
+		} else if (textbox_within(map_button, p1)) {
+			// TODO:
+		} else if (textbox_within(help_button, p1)) {
+			// TODO:
 		}
 	}
 }
