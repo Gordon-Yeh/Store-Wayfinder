8733c2e75aa72af072da88884e529171aa980389
Gordon-Yeh <gordon.yeh.dev@gmail.com>
2019-02-20 12:57:22 -0800

    add templates

---
+++ c_source/Security/tracker.h
@@ -0,0 +1,29 @@
+/**
+ * The device location security feature
+ * notifies a store employee via text message if the device has gone out the proximity of the store
+ * runs concurrently with other component of the app
+ */
+
+#ifndef TRACKER_H
+#define TRACKER_H
+
+#define IN_BOUND 0
+#define OUT_OF_BOUND 1
+
+typedef struct {
+    double x;
+    double y;   
+} gps_point;
+
+/**
+ * start the tracker concurrently
+ * @param update_rate - rate at which the location updates (in ms)
+ */
+void tracker_start(int update_rate);
+
+/**
+ * @returns the location status of the device
+ */
+int tracker_status();
+
+#endif
