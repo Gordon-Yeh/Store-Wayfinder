e874837e0e11641fcd401981b1fe8b1b21069caa
Skylar <skylarliang233@gmail.com>
2019-02-23 20:50:29 -0800

    gps parse data

--- c_source/GPS/GPS.c
+++ c_source/GPS/GPS.c
@@ -91,15 +91,28 @@ gps_point* getPoint(){
    GPSFlush();
 
    char* temp = strtok(all_data, ",");
-   char* longtitude, *latitude;
+   char* longitude, *latitude;
    for (int i=0; i<5 && temp != NULL; i++){
-      longtitude = i==2? temp : longtitude;
+      longitude = i==2? temp : longitude;
       latitude = i==4? temp : latitude;      
       temp = strtok(NULL, ",");
    }
 
-   double x = atof((const char*)longtitude);
-   double y = atof((const char*)latitude);
+   char lon_H[3], lon_M[7];
+   memcpy(lon_H, &longitude[0], 2);
+   memcpy(lon_M, &longitude[2], 6);
+   lon_H[2] = '\0';
+   lon_M[6] = '\0';
+   float h = atof((const char*)lon_H);
+   float m = atof((const char*)lon_M)/60;
+   float x = h+m;
+
+   char lat_H[4], lat_M[7];
+   memcpy(lat_H, &latitude[0], 3);
+   memcpy(lat_M, &latitude[3], 6);
+   lat_H[3] = '\0';
+   lat_M[6] = '\0';
+   float y = atof((const char*)lat_H) + atof((const char*)lat_M)/60;
 
    gps_point* p = malloc(sizeof(gps_point));
    p->lon = x;
