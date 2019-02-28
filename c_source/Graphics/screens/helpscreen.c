a507b8c94cb3873f7a89a13a8ca1712e54caa058
Gordon-Yeh <gordon.yeh.dev@gmail.com>
2019-02-27 21:40:12 -0800

    modify helpscreen

--- c_source/Graphics/screens/helpscreen.c
+++ c_source/Graphics/screens/helpscreen.c
@@ -6,7 +6,7 @@
 #include "screen.h"
 
 static const int base_x = 25;
-static const int base_y = 60;
+static const int base_y = 100;
 
 static struct {
     TextBox * back_button;
@@ -21,14 +21,11 @@ void help_screen_init() {
 
 void help_screen_draw(void) {
 	// FONT2 has height 14 and width 10
-	CenteredSentence(FONT2, 30, 789, 0, 55, FOREST_GREEN, 0, "How to use this system", DONT_ERASE);
+	CenteredSentence(FONT4, 30, 789, 0, 100, FOREST_GREEN, 0, "How to use this system", DONT_ERASE);
 	char * sentences[] = {
 		"1. Home Menu", 
-					"   - To create a new, empty shopping list, tap on the \"Create New ",
-					"     Shopping List\" button",
-					"   - To add to the existing shopping list, tap on the \"Add To ",
-					"     Shopping List\" button",
-					"   - To access a view of the store map, tap on the \"Go To Map\" button",
+					"   - To create a new shopping list, tap on the \"Start Shopping List\"",
+					"		button",
 					"   - To access this help menu, tap on the \"Help\" button",
 		"",
 		"2. Category Screen", 
@@ -47,7 +44,7 @@ void help_screen_draw(void) {
 		"4. Map Screen", 
 					"   - A map of the store is displayed with your location and the ",
 					"     locations of the items in your list"};
-	int sentences_size = 24;
+	int sentences_size = 21;
 	Paragraph(base_x, base_y, FOREST_GREEN, 0, sentences, sentences_size, DONT_ERASE);
 	
 	textbox_draw(_HelpScreen.back_button);
--- c_source/Graphics/screens/homescreen.c
+++ c_source/Graphics/screens/homescreen.c
@@ -17,21 +17,21 @@ static struct {
 
 void homescreen_init() {
     // declare and set all shapes and components on the screen here
-	TextBox * new_list_button = textbox_create(30, 60, 760, 190);
-	TextBox * help_button = textbox_create(30, 270, 760, 190);
+	TextBox * new_list_button = textbox_create(30, 150, 760, 150);
+	TextBox * help_button = textbox_create(30, 325, 760, 150);
 
 	textbox_set_box_colour(new_list_button, BLUE, FOREST_GREEN);
 	textbox_set_box_colour(help_button, BLUE, FOREST_GREEN);
 
-	textbox_set_text(new_list_button, "Start Shopping List", FONT2, WHITE);
-	textbox_set_text(help_button, "Help", FONT2, WHITE);
+	textbox_set_text(new_list_button, "Start Shopping List", FONT3, WHITE);
+	textbox_set_text(help_button, "Help", FONT3, WHITE);
 
     _HomeScreen.new_list_button = new_list_button;
     _HomeScreen.help_button = help_button;
 }
 
 void homescreen_draw() {
-	CenteredSentence(FONT2, 30, 789, 0, 55, FOREST_GREEN, 0, "Hardware Store Wayfinder", DONT_ERASE);
+	CenteredSentence(FONT5, 30, 789, 0, 150, FOREST_GREEN, 0, "Store Wayfinder", DONT_ERASE);
     textbox_draw(_HomeScreen.new_list_button);
 	textbox_draw(_HomeScreen.help_button);
 }
